ef4f2421fd1a5cdea0f5f968de382cbb180d67f6
Dolya Artem <artemdola120@gmail.com>
2025-07-22 15:06:14 +0300

    Login screen mostly completed, some other small changes to different screens.

--- screens/Login.js
+++ screens/Login.js
@@ -1,9 +1,11 @@
-import { View, Text, StyleSheet, Pressable } from "react-native";
+import { View, Text, StyleSheet, Pressable, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 
 export default function Login() {
 
   const navigation = useNavigation();
+  const [style, setStyle] = useState(styles.textOut);
 
   function navigate(screenName) {
     navigation.navigate(screenName);
@@ -16,12 +18,14 @@ export default function Login() {
   return (
     <View style={styles.container}>
       <View style={styles.loginWindow}>
-        <Text style={styles.header}></Text>
+        <Text style={styles.header}>LOGIN</Text>
+        <TextInput textAlign={"center"} multiline={true} numberOfLines={5} style={styles.textInputText} placeholder="Username"></TextInput>
+        <TextInput textAlign={"center"} multiline={true} style={styles.textInputText} placeholder="Password"></TextInput>
         <Pressable onPress={() => navigate("MainTabs")}>
-          <Text style={styles.header}>LOG IN</Text>
+          <Text style={styles.button}>LOG IN</Text>
         </Pressable>
-        <Pressable onPress={() => navigation.navigate("Register")}>
-          <Text style={styles.header}>REGISTER</Text>
+        <Pressable onPress={() => navigation.navigate("Register")} onHoverIn={() => setStyle(styles.textIn)} onHoverOut={() => setStyle(styles.textOut)}>
+          <Text style={style}>Don't have an account yet? Register</Text>
         </Pressable>
       </View>
     </View>
@@ -36,11 +40,47 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF"
   },
   loginWindow: {
+    borderRadius: 20,
+    borderColor: "#000000",
+    borderWidth: 3,
     backgroundColor: "#CC005C",
     height: "50%",
-    width: "80%"
+    width: "80%",
+    justifyContent: "center",
   },
   header: {
+    margin: 20,
+    marginTop: -10,
     fontSize: 36,
-  }
+    textAlign: "center",
+    // fontFamily: "Passion One",
+    fontWeight: "bold",
+    color: "#FFFFFF"
+  },
+  textInputText: {
+    margin: 10,
+    fontSize: 32,
+    borderRadius: 20,
+    backgroundColor: "#FFFFFF",
+    width: "85%",
+    alignSelf: "center"
+  },
+  button: {
+    margin: 20,
+    fontSize: 32,
+    alignSelf: "center",
+    color: "#FFFFFF",
+    fontWeight: "bold",
+  },
+  textOut: {
+    fontSize: 16,
+    color: "white",
+    alignSelf: "center",
+  },
+  textIn: {
+    fontSize: 16,
+    color: "white",
+    alignSelf: "center",
+    fontStyle: "italic",
+  },
 })
